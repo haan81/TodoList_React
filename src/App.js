5f6f56c058992d3e7a88fca014da88bab64e0f47
HAN-MIN LEE <han-minlee@HAN-MIN-LEEui-MacBook.local>
2019-03-09 15:30:34 +0900

    TodoList_React using LocalStorage

--- src/App.js
+++ src/App.js
@@ -7,52 +7,44 @@ class App extends Component {
 
   // 생성자에서 todos 항목
   constructor(props){
-    super(props);
+    super(props);    
+    this.RefreshTodos();
+  }
 
-
-    console.log("localStorage.length = " + localStorage.length);
+  RefreshTodos=()=>{
     if(localStorage.length!==0)
     {
       for(var ii=0; ii<localStorage.length;ii++)
       {
         var str=localStorage.getItem(ii);
 
+        console.log(ii + " : str = " + str);
         if(str !== null)
         {
           var strSplit = str.split('$');
           var bCheck=null
-          if (strSplit[1]==="0"){
+          if (strSplit[2]==="0"){
             bCheck=false;
           }
           else {
             bCheck=true;
           }
-
-          //var arrTodos=new arrTodos(ii,strSplit[0],strSplit[1]);
-          //this.state.todos.concat(arrTodos);
-
-          console.log("ii= " + ii +"\n" + "strSplit[0]= " + strSplit[0] +"\n" +bCheck);
-          this.state.todos.push(
-            {
-              id: ii,
-              text: strSplit[0],
+          this.state.todos.push({
+              id: strSplit[0],
+              text: strSplit[1],
               checked:bCheck
-            }
-          );
+          });
         }
       }
     }
     //localStorage.clear();
-    console.log("localStorage.length = " + localStorage.length);
-
   }
 
-  //id = 3 // 이미 0,1,2 가 존재하므로 3으로 설정
+  id = 0 // 이미 0,1,2 가 존재하므로 3으로 설정
 
   state = {
     input: '',
     todos: [
-
       //{ id: 0, text: ' 리액트 소개', checked: false },
       //{ id: 1, text: ' No not Key in by hangul', checked: true },
       //{ id: 2, text: ' Weight Training', checked: false }
@@ -66,26 +58,46 @@ class App extends Component {
   }
 // 눌려진 키가 Enter 면 handleCreate 호출
   handleCreate=()=>{
-
   	const{input, todos}=this.state;
-  	this.setState({
-  	  input: '', // 인풋 비우고
+    this.setState({
+      input: '', // 인풋 비우고
       // concat 을 사용하여 배열에 추가
       todos: todos.concat({
-      	id: this.id++,
-      	text: input,
-      	checked:false
+        id: this.id++,
+     	  text: input,
+     	  checked:false
       })
-  	});
+    });
 
-    console.log(this.id);
-    var TmpValue=this.state.input+"$"+0;
-    console.log(TmpValue);
+    // get id (localStorage가 0개 이상일때 마지막 저장된 id보다 1 큰 숫자)
+    if (localStorage.length===0)
+    {
+      this.id=0;  
+    }
+    else
+    {
+      // key(id)를 0부터 localStorage.length까지 검사하고 중간에 빈 숫자가 있으면 그 숫자를 id로 저장한다.
+      // localStorage.length가 끝날때까지 빈 숫자가 없으면 "localStorage.length"를 id로 저장한다.
+      var bExistEmpty=false;
+      for (var idx=0; idx<localStorage.length;idx++)
+      {
+        if(localStorage.hasOwnProperty(idx)===false)
+        {
+          this.id=idx;
+          bExistEmpty=true;
+          break;
+        }
+      }
+
+      if (bExistEmpty===false)
+      {
+        this.id= localStorage.length;  
+      }
+      
+    }
+    // localStorage에 저장할 "value" 만들고 localStorage에 저장함
+    var TmpValue=this.id+"$"+this.state.input+"$"+0;
     localStorage.setItem(this.id, TmpValue);
-
-    console.log(this.id + " : " +localStorage.getItem(this.id));
-    //localStorage.clear();
-
   }
 
   handelKeyPress=(e)=>{
@@ -102,7 +114,6 @@ class App extends Component {
     // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
     const index = todos.findIndex(todo => todo.id === id);
     const selected = todos[index]; // 선택한 객체
-
     const nextTodos = [...todos]; // 배열을 복사
 
     // 기존의 값들을 복사하고, checked 값을 덮어쓰기
@@ -114,13 +125,85 @@ class App extends Component {
     this.setState({
       todos: nextTodos
     });
+
+
+    // Toggle 된 아이템의 값을 localstorage에도 업데이트 한다
+    var TmpArray=[];
+
+    for(var ii=0; ii<localStorage.length;ii++)
+    {
+      var Tmpstr=localStorage.getItem(ii);
+      var strSplit = Tmpstr.split('$');
+
+      if (id===strSplit[0])
+      {
+        if (strSplit[2]==='1')
+        {
+          strSplit[2]=0;
+        }
+        else if (strSplit[2]==='0')
+        {
+          strSplit[2]=1;
+        }
+
+        Tmpstr=strSplit[0]+"$"+strSplit[1]+"$"+strSplit[2];
+        console.log("id = " + id+ " // ChagedStr = " + Tmpstr);
+      }
+      
+      // LocalStorage에 저장하기 위해 TmpArray에 저장함. 
+      TmpArray[ii]=Tmpstr;
+      
+    }
+
+    // 2. localstorage를 clear 했다가 다시 정의함.
+    this.UpdateLocalStorage(TmpArray);
   }
 
   handleRemove=(id)=>{
-  	const {todos}=this.state;
-  	this.setState({
-  		todos: todos.filter(todo=>todo.id!==id)
-  	});
+    console.log("selected id = " + id);
+
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    });
+
+    // 1. localstorage에서 id값을 가진 data를 뺀 dataSet을 별도 array에 저장하고
+    if(localStorage.length!==0)
+    {
+      var TmpArr=[];
+      var TmpArrIdx=0;
+
+      for(var ii=0; ii<localStorage.length;ii++)
+      {
+        var Tmpstr=localStorage.getItem(ii);
+        var strSplit = Tmpstr.split('$');
+
+
+        if (id!==strSplit[0])
+        {
+          TmpArr[TmpArrIdx]=localStorage.getItem(ii);
+          TmpArrIdx=TmpArrIdx+1;
+        }
+      }
+
+    }
+
+    // 2. localstorage를 clear 했다가 다시 정의함.
+    this.UpdateLocalStorage(TmpArr);
+    this.RefreshTodos();
+    //window.location.reload();
+    
+  }
+
+  UpdateLocalStorage=(TmpArr)=>{
+    localStorage.clear();
+
+    for(var idx=0; idx<TmpArr.length;idx++)
+    {
+      var str=TmpArr[idx];
+      localStorage.setItem(idx,str);
+    }
+
   }
 
 
@@ -137,8 +220,6 @@ class App extends Component {
   		handleToggle,
   		handleRemove
   	}=this;
-
-
 
     return (
       <TodoListTemplate form={(
